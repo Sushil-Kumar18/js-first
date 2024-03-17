c266b4db2053da04702b85b2fc72bea09f0460ba
Sushil-Kumar18 <163150105+Sushil-Kumar18@users.noreply.github.com>
2024-03-17 17:47:27 +0000

    variable file added

---
+++ 01_basics/variable.js
@@ -0,0 +1,5 @@
+const accountId = 12232
+let accountEmail = "broken@gmail.com"
+var accountPassword = "23243"
+accountCity = "Raipur"
+console.table([accountId, accountEmail, accountPassword, accountCity])
