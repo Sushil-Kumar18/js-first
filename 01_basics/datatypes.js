5b41bf45beb2ea26d1a074b083cb9543606d6275
Sushil-Kumar18 <163150105+Sushil-Kumar18@users.noreply.github.com>
2024-03-18 17:36:14 +0000

    data files added

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; /// This will treat all JS as newer version
+// alert(3 +3)  wrong as we are using nodejs, not brower
+console.log(3 + 
+    3)  /// This is the wrong method as it will decrease the code readability
+
+console.log(5 +5)  /// This is the right way
+
+let name = "Sushil"
+let age = 34
+let isLogged = true
+console.log(typeof name);
