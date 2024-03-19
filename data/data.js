72561f92c7193eb368bf5fb5c1b1474f7f5cb593
Sushil-Kumar18 <163150105+Sushil-Kumar18@users.noreply.github.com>
2024-03-19 16:08:06 +0000

    data conversion

---
+++ data/data.js
@@ -0,0 +1,15 @@
+let score = undefined
+console.log(typeof score);
+console.log(typeof (score));
+
+let isConvert = Number(score)
+console.log(typeof isConvert);
+console.log(isConvert);
+
+
+
+let isloggin = 1
+console.log(typeof isloggin);
+
+let boolean = Boolean(isloggin)
+console.log(boolean);
